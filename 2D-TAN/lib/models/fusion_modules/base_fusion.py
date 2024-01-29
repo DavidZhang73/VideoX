03651c18c2279cf434f8b7ca61c02482ccfeb624
DavidZ <davidbill@outlook.com>
2024-01-29 17:23:41 +1100

    ✨ Shortcut lstm

--- 2D-TAN/lib/models/fusion_modules/base_fusion.py
+++ 2D-TAN/lib/models/fusion_modules/base_fusion.py
@@ -10,20 +10,21 @@ class BaseFusion(nn.Module):
         hidden_size = cfg.HIDDEN_SIZE
         txt_input_size = cfg.TXT_INPUT_SIZE
         txt_hidden_size = cfg.TXT_HIDDEN_SIZE
-        self.textual_encoder = nn.LSTM(
-            txt_input_size,
-            txt_hidden_size // 2 if cfg.LSTM.BIDIRECTIONAL else txt_hidden_size,
-            num_layers=cfg.LSTM.NUM_LAYERS,
-            bidirectional=cfg.LSTM.BIDIRECTIONAL,
-            batch_first=True,
-        )
+        # self.textual_encoder = nn.LSTM(
+        #     txt_input_size,
+        #     txt_hidden_size // 2 if cfg.LSTM.BIDIRECTIONAL else txt_hidden_size,
+        #     num_layers=cfg.LSTM.NUM_LAYERS,
+        #     bidirectional=cfg.LSTM.BIDIRECTIONAL,
+        #     batch_first=True,
+        # )
         self.tex_linear = nn.Linear(txt_hidden_size, hidden_size)
         self.vis_conv = nn.Conv2d(hidden_size, hidden_size, 1, 1)
 
     def forward(self, textual_input, textual_mask, map_h, map_mask):
-        self.textual_encoder.flatten_parameters()
-        txt_h = self.textual_encoder(textual_input)[0] * textual_mask
-        txt_h = torch.stack([txt_h[i][torch.sum(mask).long() - 1] for i, mask in enumerate(textual_mask)])
+        # self.textual_encoder.flatten_parameters()
+        # txt_h = self.textual_encoder(textual_input)[0] * textual_mask
+        # txt_h = torch.stack([txt_h[i][torch.sum(mask).long() - 1] for i, mask in enumerate(textual_mask)])
+        txt_h = textual_input
         txt_h = self.tex_linear(txt_h)[:, :, None, None]
         map_h = self.vis_conv(map_h)
         fused_h = F.normalize(txt_h * map_h) * map_mask
